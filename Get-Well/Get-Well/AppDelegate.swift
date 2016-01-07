4a55efe60411f030b1c7d7bfad34ccda465054b0
Elizabeth Elliott <elliott1006@gmail.com>
2016-01-06 23:05:19 -0500

    commit

--- Get-Well/Get-Well/AppDelegate.swift
+++ Get-Well/Get-Well/AppDelegate.swift
@@ -15,13 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
-    {
-        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Alert, .Sound], categories: nil))
-        
-        Parse.setApplicationId("VlKJB7hFnBgEIp5Z9eIFela9YRhKXQoxriKWlJO9",
-            clientKey: "2bMSiwEwy31B43dJrnEO7vQfVLZrNYSK7wDbZ8U9")
-        
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
         return true
     }
 
